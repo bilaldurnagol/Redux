7fd505d1debdcbdfe7b3768893f9558495458b28
Bilal Durnagöl <bilaldurnagol@gmail.com>
2023-04-08 02:24:11 +0300

    Initial Commit

---
+++ Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by Bilal Durnagöl on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
